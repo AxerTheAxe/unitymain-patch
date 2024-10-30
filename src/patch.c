32fcedfddeb9c15d426cc8bd40d6f77b236db498
Katherine Jamison <axertheaxe@proton.me>
2024-10-30 02:08:59 -0600

    refactor: improve naming

--- src/patch.c
+++ src/patch.c
@@ -2,7 +2,7 @@
 #include <windows.h>
 
 #define UNITY_DLL "UnityPlayer.dll"
-#define UNITY_MAIN "UnityMain"
+#define UNITY_MAIN_FUNC "UnityMain"
 
 #define eprintln(msg, ...) fprintf(stderr, msg"\n", ##__VA_ARGS__)
 
@@ -11,9 +11,9 @@ typedef int (*UnityFuncPtr)(HINSTANCE, HINSTANCE, LPWSTR, int);
 int WINAPI wWinMain(const HINSTANCE hInstance, const HINSTANCE hPrevInstance, const LPWSTR lpCmdLine, const int nCmdShow) {
     const HMODULE unityDll = LoadLibraryA(UNITY_DLL);
     if (!unityDll) {
-        const DWORD error = GetLastError();
+        const DWORD dllError = GetLastError();
         
-        switch (error) {
+        switch (dllError) {
             case ERROR_MOD_NOT_FOUND:
                 eprintln("Could not find '%s'.", UNITY_DLL);
                 break;
@@ -21,22 +21,22 @@ int WINAPI wWinMain(const HINSTANCE hInstance, const HINSTANCE hPrevInstance, co
                 eprintln("'%s' is not a valid DLL.", UNITY_DLL);
                 break;
             default:
-                eprintln("Failed to load '%s'. Error code '%lu'.", UNITY_DLL, error);
+                eprintln("Failed to load '%s'. Error code '%lu'.", UNITY_DLL, dllError);
                 break;
         };
 
         return EXIT_FAILURE;
     }
 
-    UnityFuncPtr UnityMain = (UnityFuncPtr)GetProcAddress(unityDll, UNITY_MAIN);
+    UnityFuncPtr UnityMain = (UnityFuncPtr)GetProcAddress(unityDll, UNITY_MAIN_FUNC);
     if (!UnityMain) {
-        eprintln("Could not get the process address of the '%s' function.", UNITY_MAIN);
+        eprintln("Could not get the process address of the '%s' function.", UNITY_MAIN_FUNC);
         FreeLibrary(unityDll);
         return EXIT_FAILURE;
     }
 
-    const int unityExit = UnityMain(hInstance, hPrevInstance,  lpCmdLine, nCmdShow);
+    const int unityExitCode = UnityMain(hInstance, hPrevInstance,  lpCmdLine, nCmdShow);
     FreeLibrary(unityDll);
 
-    return unityExit;
+    return unityExitCode;
 }
