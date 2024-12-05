6de523888352cb30b6bbd45df1104463242b0b8b
Katherine Jamison <axertheaxe@proton.me>
2024-12-05 12:28:49 -0700

    refactor(flake.nix): refactor for correctness

--- flake.nix
+++ flake.nix
@@ -1,28 +1,24 @@
 {
-    description = "Flake to build on NixOS.";
+    description = "Flake to source development environment with Nix.";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     };
 
     outputs =
         { nixpkgs, ... }:
         let
             system = "x86_64-linux";
-            pkgs = nixpkgs.legacyPackages.${system};
+            pkgs = import nixpkgs { localSystem = system; };
         in
         {
-            devShells.${system}.default = pkgs.mkShell {
-                packages = [ pkgs.wine64 ];
-                depsBuildBuild = [
-                    (pkgs.pkgsCross.mingwW64.stdenv.cc.override ({
-                        extraBuildCommands = ''
-                            printf '%s' ' -L${pkgs.pkgsCross.mingwW64.windows.mcfgthreads}/lib' >> $out/nix-support/cc-ldflags
-                            printf '%s' ' -I${pkgs.pkgsCross.mingwW64.windows.mcfgthreads.dev}/include' >> $out/nix-support/cc-cflags
-                        '';
-                    }))
-                    pkgs.gnumake
-                ];
-            };
+            devShells.${system}.default =
+                with pkgs;
+                mkShell.override { stdenv = pkgsCross.mingwW64.stdenv; } {
+                    nativeBuildInputs = [
+                        wine64
+                        bear
+                    ];
+                };
         };
 }
