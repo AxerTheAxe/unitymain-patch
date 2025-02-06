9c47eed6d5e6f42636cc3e36be7cdae03a4b8298
Katherine Jamison <axertheaxe@proton.me>
2025-02-06 00:28:59 -0700

    refactor: rewrite from scratch

--- src/patch.c
+++ src/patch.c
@@ -1,42 +1,82 @@
 #include <stdio.h>
 #include <windows.h>
 
-#define UNITY_DLL "UnityPlayer.dll"
-#define UNITY_MAIN_FUNC "UnityMain"
+#define UNITY_PLAYER "UnityPlayer.dll"
+#define UNITY_MAIN "UnityMain"
 
-#define eprintln(msg, ...) fprintf(stderr, msg"\n", ##__VA_ARGS__)
+typedef int (*UnityMain)(HINSTANCE, HINSTANCE, LPWSTR, int);
 
-typedef int (*UnityFuncPtr)(HINSTANCE, HINSTANCE, LPWSTR, int);
+static inline void logBasic(
+    FILE* const outputFile, const char* const prefix, const char* const message,
+    const va_list arguments
+) {
+    fprintf(outputFile, "%s", prefix);
+    vfprintf(outputFile, message, arguments);
+}
 
-int WINAPI wWinMain(const HINSTANCE hInstance, const HINSTANCE hPrevInstance, const LPWSTR lpCmdLine, const int nCmdShow) {
-    const HMODULE unityDll = LoadLibraryA(UNITY_DLL);
-    if (!unityDll) {
-        const DWORD dllError = GetLastError();
-        
-        switch (dllError) {
+static void logMessage(const char* const message, ...) {
+    va_list arguments = NULL;
+    va_start(arguments, message);
+
+    logBasic(stdout, "unitymain-patch: ", message, arguments);
+
+    va_end(arguments);
+}
+
+static void logError(const char* const message, ...) {
+    va_list arguments = NULL;
+    va_start(arguments, message);
+
+    logBasic(stderr, "error: ", message, arguments);
+
+    va_end(arguments);
+}
+
+int WINAPI
+wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, const int nCmdShow) {
+    const HMODULE unityPlayer = LoadLibraryA(UNITY_PLAYER);
+    if (!unityPlayer) {
+        const DWORD dllLoadError = GetLastError();
+        switch (dllLoadError) {
             case ERROR_MOD_NOT_FOUND:
-                eprintln("Could not find '%s'.", UNITY_DLL);
+                logError("could not find the Unity player DLL '%s'\n", UNITY_PLAYER);
                 break;
             case ERROR_BAD_EXE_FORMAT:
-                eprintln("'%s' is not a valid DLL.", UNITY_DLL);
+                logError(
+                    "could not parse the Unity player DLL '%s' as a valid DLL\n", UNITY_PLAYER
+                );
                 break;
             default:
-                eprintln("Failed to load '%s'. Error code '%lu'.", UNITY_DLL, dllError);
-                break;
-        };
-
-        return EXIT_FAILURE;
+                logError(
+                    "failed to load the Unity player DLL '%s'. Returning with code "
+                    "'%lu'\n",
+                    UNITY_PLAYER, dllLoadError
+                );
+        }
+        return dllLoadError;
     }
+    logMessage("successfully loaded the Unity player DLL '%s'\n", UNITY_PLAYER);
 
-    UnityFuncPtr UnityMain = (UnityFuncPtr)GetProcAddress(unityDll, UNITY_MAIN_FUNC);
-    if (!UnityMain) {
-        eprintln("Could not get the process address of the '%s' function.", UNITY_MAIN_FUNC);
-        FreeLibrary(unityDll);
-        return EXIT_FAILURE;
+    const UnityMain unityMain = (UnityMain)(uintptr_t)GetProcAddress(unityPlayer, "UnityMain");
+    if (!unityMain) {
+        const DWORD addressError = GetLastError();
+
+        logError(
+            "error: failed to retrieve the function '%s' from the Unity "
+            "player DLL '%s'\n",
+            UNITY_MAIN, UNITY_PLAYER
+        );
+
+        FreeLibrary(unityPlayer);
+        return addressError;
     }
+    logMessage(
+        "unitymain-patch: successfully retrieved the function '%s' from "
+        "the Unity player DLL '%s'\n",
+        UNITY_MAIN, UNITY_PLAYER
+    );
 
-    const int unityExitCode = UnityMain(hInstance, hPrevInstance,  lpCmdLine, nCmdShow);
-    FreeLibrary(unityDll);
-
-    return unityExitCode;
+    const int unityMainReturn = unityMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+    FreeLibrary(unityPlayer);
+    return unityMainReturn;
 }
